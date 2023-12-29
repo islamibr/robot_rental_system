01d9fbe3556f7f7f324ed70802196d1d7795768f
Islam Ibrahim <49861069+islamibr@users.noreply.github.com>
2023-12-29 11:46:54 +0200

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,8 +1,10 @@
 #include "User.h"
 
+// Constructor
 User::User(std::string name, std::string telephone_number, std::string email)
     : name(name), telephone_number(telephone_number), email(email) {}
 
+// Getter methods
 std::string User::getName() const {
     return name;
 }
@@ -19,6 +21,7 @@ std::vector<std::string> User::getRobotsRented() const {
     return robots_rented;
 }
 
+// Other methods
 void User::rentRobot(const std::string& robot_name) {
     robots_rented.push_back(robot_name);
 }
