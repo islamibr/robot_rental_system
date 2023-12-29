96e1719ff33fe265f973d0b60d00d220f54aca8b
Islam Ibrahim <49861069+islamibr@users.noreply.github.com>
2023-12-29 11:46:24 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,8 +1,10 @@
 #include "Robot.h"
 
+// Constructor
 Robot::Robot(std::string name, std::vector<int> function_ids, double price_per_day)
     : name(name), function_ids(function_ids), price_per_day(price_per_day), rented(false) {}
 
+// Getter methods
 std::string Robot::getName() const {
     return name;
 }
@@ -23,10 +25,12 @@ std::vector<std::string> Robot::getDatesRented() const {
     return dates_rented;
 }
 
+// Setter method
 void Robot::setRented(bool status) {
     rented = status;
 }
 
+// Other methods
 void Robot::addDateRented(const std::string& date) {
     dates_rented.push_back(date);
 }
